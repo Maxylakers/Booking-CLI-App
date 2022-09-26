d3e101edaad32c1793a69b391483c5f51e8d46a3
Revolver <maxylakers@gmail.com>
2022-09-26 20:46:51 +0100

    feat: Add simple booking logic

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ func main() {
 	fmt.Println("How many tickets do you want to book? ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("We still have: %v tickets remaining for the %v", remainingTickets, conferenceName)
 
 }
