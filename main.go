4175305bf4f5921f23c795cd7e18b1e8a0720269
Revolver <maxylakers@gmail.com>
2022-09-26 23:11:39 +0100

    Feat: Return only users' first name for bookings

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	conferenceName := "Max Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
-	var bookings []string
+	bookings := []string{}
 
 	fmt.Printf("Welcome To %v Booking Application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets, and %v tickets are still remaining\n", conferenceTickets, remainingTickets)
@@ -36,12 +37,45 @@ func main() {
 	// bookings[0] = firstName + " " + lastName
 	bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Content of the whole array: %v\n", bookings)
-	fmt.Printf("First value of the array: %v\n", bookings[0])
-	fmt.Printf("Type of the array: %T\n", bookings)
-	fmt.Printf("Length of the array: %v\n", len(bookings))
-
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("We still have: %v tickets remaining for the %v", remainingTickets, conferenceName)
+	fmt.Printf("We still have: %v tickets remaining for the %v\n", remainingTickets, conferenceName)
+
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		fmt.Println("How many tickets do you want to book? ")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+
+		// bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("We still have: %v tickets remaining for the %v\n", remainingTickets, conferenceName)
+
+		// To use the for each statement, we need the "index" and the element declared. But since we are not using the index variable in the program, we will replace it with an "_"
+		firstNames := []string{}
+		for _, booking := range bookings {
+			// We will use the strings.Fields builtin function to seperate each element in booking with whitespaces
+			names := strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("The first names of all bookings are:  %v\n", firstNames)
+
+	}
 
 }
