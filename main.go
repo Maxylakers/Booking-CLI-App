306aeedcc54af6db42628bc2bb5721186c31aab0
Revolver <maxylakers@gmail.com>
2022-09-27 00:15:06 +0100

    Refactor: error handling for booking logic

--- main.go
+++ main.go
@@ -15,31 +15,6 @@ func main() {
 	fmt.Printf("We have a total of %v tickets, and %v tickets are still remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get Your Tickets Here Now To Attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
-
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
-
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
-
-	fmt.Println("How many tickets do you want to book? ")
-	fmt.Scan(&userTickets)
-
-	remainingTickets = remainingTickets - userTickets
-
-	// bookings[0] = firstName + " " + lastName
-	bookings = append(bookings, firstName+" "+lastName)
-
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("We still have: %v tickets remaining for the %v\n", remainingTickets, conferenceName)
-
 	for {
 		var firstName string
 		var lastName string
@@ -58,23 +33,35 @@ func main() {
 		fmt.Println("How many tickets do you want to book? ")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
+		if userTickets <= remainingTickets {
+			var remainingTickets uint = remainingTickets - userTickets
 
-		// bookings[0] = firstName + " " + lastName
-		bookings = append(bookings, firstName+" "+lastName)
+			// bookings[0] = firstName + " " + lastName
+			bookings = append(bookings, firstName+" "+lastName)
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("We still have: %v tickets remaining for the %v\n", remainingTickets, conferenceName)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("We now have: %v tickets remaining for the %v\n", remainingTickets, conferenceName)
 
-		// To use the for each statement, we need the "index" and the element declared. But since we are not using the index variable in the program, we will replace it with an "_"
-		firstNames := []string{}
-		for _, booking := range bookings {
-			// We will use the strings.Fields builtin function to seperate each element in booking with whitespaces
-			names := strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			// To use the for each statement, we need the "index" and the element declared. But since we are not using the index variable in the program, we will replace it with an "_"
+			firstNames := []string{}
+			for _, booking := range bookings {
+				// We will use the strings.Fields builtin function to seperate each element in booking with whitespaces
+				names := strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("The first names of all bookings are:  %v\n", firstNames)
+
+			noTicketsRemaining := remainingTickets == 0
+			if noTicketsRemaining {
+				// end the program
+				fmt.Printf("Our tickets are booked out. Come back next year")
+				break
+			}
+		} else {
+
+			fmt.Printf("We currently have only %v number of tickets remaining, you cannot book %v tickets\n", remainingTickets, userTickets)
 		}
-
-		fmt.Printf("The first names of all bookings are:  %v\n", firstNames)
 
 	}
 
